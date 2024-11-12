e8077626aa56fc06b3998464cc3912988ab70668
Oenothera <wjsj1015@naver.com>
2024-11-12 19:29:46 +0900

    Feat : react-unity-webgl 라이브러리 추가, Canvas 구현
    1. react-unity-webgl 라이브러리 추가
    - react-unity-webgl 라이브러리 추가
    2. Canvas 구현
    - unity 프로젝트 webgl 빌드 데이터 public에 추가
    - react-unity-webgl을 활용한 Canvas 컴포넌트 구현
    3. GNB 및 페이지 css 조정
    - GNB 항상 상위 노출하도록 구현
    - 페이지 css 의 기본 padding값 설정

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Canvas from "@/components/Canvas/Canvas";
 
 export default function Home() {
-  return <div className={styles.page_container}></div>;
+  return <div className={styles.page_container}>{<Canvas />}</div>;
 }
---
+++ front/src/components/Canvas/Canvas.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { ReactElement, useEffect, useState } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+export default function Canvas() {
+  const [canvas, setCanvas] = useState<ReactElement>();
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/webgl/Build/webgl.loader.js",
+    dataUrl: "/webgl/Build/webgl.data",
+    frameworkUrl: "/webgl/Build/webgl.framework.js",
+    codeUrl: "/webgl/Build/webgl.wasm",
+  });
+
+  useEffect(() => {
+    const renderUnityCanvas = (): ReactElement => {
+      return (
+        <>
+          <Unity
+            unityProvider={unityProvider}
+            style={{ width: 500, height: 500 }}
+          />
+        </>
+      );
+    };
+    const renderedCanvas: ReactElement = renderUnityCanvas();
+    setCanvas(renderedCanvas);
+  }, []);
+
+  return <>{canvas ? canvas : null}</>;
+}
